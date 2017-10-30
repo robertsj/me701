e3102a00b26e795b840811ed8620f436fe8d527a
Jeremy <jaroberts@ksu.edu>
2017-10-30 08:39:06 -0500

    prep c++ arrays

---
+++ examples/compiled/basic_arrays_functions.cc
@@ -0,0 +1,36 @@
+#include "basic_arrays_functions.hh"
+
+#include <cstdio>
+
+// Demonstrate how to pass dumb arrays.  Note, prefer to "const" all 
+// incoming scalars (ints, floats, etc.) that are not to change inside the
+// function.  Pedantic, but "defensive"
+void passing_dumb_arrays(double *a, const int n)
+{
+    // do something with the array
+    std::printf("a[1] = %6.2f\n", a[1]);
+}
+
+void pass_vector_by_value(vec_dbl a)
+{
+    // do something with the array
+    std::printf("a[1] = %6.2f\n", a[1]);
+    // change an element
+    a[1] = 99;
+}
+
+void pass_vector_by_reference(vec_dbl &a)
+{
+    // do something with the array
+    std::printf("a[1] = %6.2f\n", a[1]);
+    // change an element
+    a[1] = 99;
+}
+
+void pass_vector_by_pointer(vec_dbl *a)
+{
+    // do something with the array
+    std::printf("a[1] = %6.2f\n", (*a)[1]);
+    // change an element
+    (*a)[1] = 101;
+}
