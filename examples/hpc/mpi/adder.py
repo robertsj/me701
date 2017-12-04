8638bf45cd40f28bbc4a62cdc8e247870bfa204c
Jeremy <jaroberts@ksu.edu>
2017-12-04 11:26:51 -0600

    more mpi

--- examples/hpc/mpi/adder.py
+++ examples/hpc/mpi/adder.py
@@ -2,32 +2,29 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
+# Start timer
 t0 = MPI.Wtime()
-
 # Sum up i = 1....n
 n = int(1e7)
-
 # Choose my bounds
 chunk = n//size
 b, e = rank*chunk+1, (rank+1)*chunk+1
 if rank == size - 1:
     e = n+1
-
 # Do my sum
 s = 0
 for i in range(b, e):
     s += i
-
 # Get them all to master
 for i in range(1, size):
     if rank == i:
         comm.send(s, dest=0, tag=i)    
     elif rank == 0:
         s += comm.recv(source=i, tag=i)
-    
+# Print out the result  
 if rank == 0:
-    print("sum = {}, expected = {}, time = {}".format(s, (n+1)*n/2, MPI.Wtime()-t0))
+    tmpl="sum = {}, expected = {}, time = {}"
+    print(tmpl.format(s, (n+1)*n/2, MPI.Wtime()-t0))
       
   
   
