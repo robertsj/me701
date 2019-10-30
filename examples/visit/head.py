76d87fd43cae6d90e3491c952d85bb27a059475e
Jeremy Roberts <jaroberts@ksu.edu>
2019-10-30 09:55:20 -0500

    Modified head example

--- examples/visit/head.py
+++ examples/visit/head.py
@@ -1,11 +1,13 @@
 # need to run with Python 2  (e.g., python2)
+#export PATH=$PATH:/home/robertsj/opt/visit/visit3_0_2.linux-x86_64/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/robertsj/opt/visit/visit3_0_2.linux-x86_64/3.0.2/linux-x86_64/lib
 import sys
-sys.path.append("/home/robertsj/opt/visit/visit2_13_2.linux-x86_64/2.13.2/linux-x86_64/lib/site-packages/")
+sys.path.append("/home/robertsj/opt/visit/visit3_0_2.linux-x86_64/3.0.2/linux-x86_64/lib/site-packages/visit")
 import visit
 visit.Launch()
 visit.ShowAllWindows()
 visit.OpenDatabase("/home/robertsj/opt/visit/visit_data_files/CThead_mid.silo")
-visit.AddPlot("Volume", "head")
+visit.AddPlot("Pseudocolor", "head")
 
 visit.AddOperator("Slice")
 s = visit.SliceAttributes()
@@ -16,4 +18,4 @@ visit.SetOperatorOptions(s)
 visit.DrawPlots()
 
 
-visit.SaveWindow()
+#visit.SaveWindow()
